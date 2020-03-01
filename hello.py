a47dc0e4bfe6f31fbe6372647c21673b604589e5
pythoninthegrass <lance.stephens@okstate.edu>
2020-02-29 22:34:05 -0700

    Hello world!

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+print("Hello world!")
+print("What is your name?")
+my_name = input()
+
+print("It's good to meet you, " + my_name)
+print("The length of your name is: ")
+print(len(my_name))
+
+print("What is your age?")
+my_age = input()
+print("You will be " + str(int(my_age) + 1) + " in a year. ")
