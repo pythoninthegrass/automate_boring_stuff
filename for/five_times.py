36b69d2e0646b877d1c7c6fc6242a1a8f36efe38
pythoninthegrass <lance.stephens@okstate.edu>
2020-03-03 21:27:22 -0700

    folders. for + while loops

---
+++ for/five_times.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# 5x for loop
+# print('My name is ')
+# for i in range(5):
+#     print("Jimmy Five Times " + str(i))
+
+# Gauss' for loop
+# total = 0
+# for num in range(101):
+#     total = total + num
+# print(total)
+
+# 5x while loop
+# print('My name is ')
+# i = 0
+# while i < 5:
+#     print("Jimmy Five Times " + str(i))
+#     i = i + 1
+
+# 5x for loop w/range(start, stop, step)
+print('My name is ')
+# for i in range(0, 10, 2):             # 2, 4, 6, 8
+for i in range(5, -1, -1):              # 5, 4, 3, 2, 1, 0
+    print("Jimmy Five Times " + str(i))
