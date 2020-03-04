c45aa0f469a3f7cfe047ef8795050941c55a9f69
pythoninthegrass <lance.stephens@okstate.edu>
2020-03-03 21:47:07 -0700

    Built-in functions

---
+++ functions/built-in_functions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# import random
+
+# call module followed by its function
+# random.randint(1, 10)
+
+# from random import *
+
+# call function w/o module
+# non-standard usage and less preferred than module + function
+# randint(1, 10)
+
+# import sys
+# print('Hello ')
+# sys.exit
+# print('Goodbye ')   # never runs ^^
+
+# NON-STD LIBRARY
+# pip install pyperclip
+import pyperclip
+pyperclip.copy('Hello world! ')
+pyperclip.paste()
