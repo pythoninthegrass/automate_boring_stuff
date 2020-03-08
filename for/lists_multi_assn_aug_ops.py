9bdff16bc97345d7bb38621bf069a0ab61a23cbc
pythoninthegrass <lance.stephens@okstate.edu>
2020-03-08 14:03:38 -0600

    for loops w/lists, multiple assignment, and augmented operators

---
+++ for/lists_multi_assn_aug_ops.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# supplies = ['pens', 'staples', 'flamethrowers', 'binders']
+# for i in range(len(supplies)):
+#     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+cat = ['fat', 'orange', 'loud']
+# size = cat[0]
+# color = cat[1]
+# disposition = cat[2]
+size, color, disposition = cat
+# size, color, dispositon = 'skinny', 'black', 'quiet'
+print(cat)
+
+# multiple assignment
+# a = 'AAA'
+# b = 'BBB'
+# a, b = b, a
+
+# augmented assignment operators
+# += 1 # spam = spam + 1
+# -= 1 # spam = spam - 1
+# *= 1 # spam = spam * 1
+# /= 1 # spam = spam / 1
+# %= 1 # spam = spam % 1
+spam = 42
+spam += 1
+print(spam)
